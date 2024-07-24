1ba4881c12ffdf57c629497717215ba56f5ccffe
Sharath <58863091+sbhaskara-ca@users.noreply.github.com>
2024-07-23 22:05:47 -0700

    added sh to the commands

--- vars/postCommits.groovy
+++ vars/postCommits.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]){
-    touch commit_log
-    git log --pretty=format:"%H %ad %s" --date=format:"%Y-%m-%d %H:%M:%S" ${env.GIT_PREVIOUS_COMMIT}..${env.GIT_COMMIT} > commit_log
-    cat commit_log
+    sh 'touch commit_log'
+    sh 'git log --pretty=format:"%H %ad %s" --date=format:"%Y-%m-%d %H:%M:%S" ${env.GIT_PREVIOUS_COMMIT}..${env.GIT_COMMIT} > commit_log'
+    sh 'cat commit_log'
 }
