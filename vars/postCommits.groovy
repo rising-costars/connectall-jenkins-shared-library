1640830b69691965ac50d3dc7248e4d0f18231b5
Sharath <58863091+sbhaskara-ca@users.noreply.github.com>
2024-07-24 12:56:15 -0700

    replaced quotes

--- vars/postCommits.groovy
+++ vars/postCommits.groovy
@@ -1,30 +1,30 @@
 def call(Map config = [:]){
     sh "touch ${config.GitRepoLoc}/commit_log"
-    sh "git --git-dir=${config.GitRepoLoc}/.git log --pretty=format:'%H %ad' --date=iso ${config.PrevSuccessBuildCommit}..${config.CurrentBuildCommit} > ${config.GitRepoLoc}/commit_log"
+    sh "git --git-dir=${config.GitRepoLoc}/.git log --pretty=format:"%H %ad" --date=iso ${config.PrevSuccessBuildCommit}..${config.CurrentBuildCommit} > ${config.GitRepoLoc}/commit_log"
     sh "echo >> ${config.GitRepoLoc}/commit_log"
     sh "cat ${config.GitRepoLoc}/commit_log"
-    sh """
+    sh '''\
     #!/bin/bash
 
-    _AUTOMATION_NAME=${config.AutomationName}
-    _DEPLOY_ID=${config.DeployId}
+    _AUTOMATION_NAME="${config.AutomationName}"
+    _DEPLOY_ID="${config.DeployId}"
 
-    _GIT_REPO=${config.GitRepoLoc}
-    _GIT_PREV_COMMIT=${config.PrevSuccessBuildCommit}
+    _GIT_REPO="${config.GitRepoLoc}"
+    _GIT_PREV_COMMIT="${config.PrevSuccessBuildCommit}"
     _GIT_CURR_COMMIT=${config.CurrentBuildCommit}
 
-    _CONNECTALL_UA_URL=${config.ConnectALL_Api_Url}
-    _CONNECTALL_API_KEY=${config.ConnectALL_Api_Key}
+    _CONNECTALL_UA_URL="${config.ConnectALL_Api_Url}"
+    _CONNECTALL_API_KEY="${config.ConnectALL_Api_Key}"
 
-    #echo 'Automation Name : \$_AUTOMATION_NAME'
+    #echo "Automation Name : \$_AUTOMATION_NAME"
 
     # Reading each line from the file
     while IFS= read -r input_text; do
         # Splitting the input text by space to separate commit ID and timestamp
         read -r commit_id timestamp <<< \$input_text
 
-        formatted_date=\$(date -d \"\$timestamp\" +'%Y-%m-%dT%H:%M:%S%z')
-        #formatted_date=\$(date -j -f '%Y-%m-%d %H:%M:%S %z' '\$timestamp' +'%Y-%m-%dT%H:%M:%S%z')
+        formatted_date=\$(date -d \"\$timestamp\" +"%Y-%m-%dT%H:%M:%S%z")
+        #formatted_date=\$(date -j -f "%Y-%m-%d %H:%M:%S %z" "\$timestamp" +"%Y-%m-%dT%H:%M:%S%z")
 
         #json="{\"appLinkName\":\"\$_AUTOMATION_NAME\",\"fields\": {\"CommitId\":\"\$commit_id\",\"CommitTimestamp\":\"\$formatted_date\",\"DeployId\": \"\$_DEPLOY_ID\"}}"
         json=$(cat <<EOF
@@ -40,10 +40,10 @@ def call(Map config = [:]){
             )
         
         # Post to connectall
-        curl --header 'Content-Type: application/json;charset=UTF-8' -X POST -d \"\$json\" \$_CONNECTALL_UA_URL/connectall/api/2/postRecord?apikey=\$_CONNECTALL_API_KEY
+        curl --header "Content-Type: application/json;charset=UTF-8" -X POST -d \"\$json\" \$_CONNECTALL_UA_URL/connectall/api/2/postRecord?apikey=\$_CONNECTALL_API_KEY
       
       
-    done < ${config.GitRepoLoc}/commit_log
-    """
-    sh 'echo Completed'
+    done < "${config.GitRepoLoc}/commit_log"
+    '''
+    sh "echo Completed"
 }   
