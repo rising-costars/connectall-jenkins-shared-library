95afc2751151a676ae09d1814376578686845d3a
Sharath <58863091+sbhaskara-ca@users.noreply.github.com>
2024-07-23 22:26:21 -0700

    change date format to iso

--- vars/postCommits.groovy
+++ vars/postCommits.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]){
     sh 'touch commit_log'
-    sh 'git log --pretty=format:"%H %ad %s" --date=iso8601-strict $GIT_PREVIOUS_COMMIT..$GIT_COMMIT > commit_log'
+    sh 'git log --pretty=format:"%H %ad" --date=iso $GIT_PREVIOUS_COMMIT..$GIT_COMMIT > commit_log'
     sh 'cat commit_log'
     sh '''
     #!/bin/bash
