3ef1aa93019427121d4dfbb61f7647e28f0d49e1
Sharath <58863091+sbhaskara-ca@users.noreply.github.com>
2024-07-23 22:36:14 -0700

    formatted date fix

--- vars/postCommits.groovy
+++ vars/postCommits.groovy
@@ -1,6 +1,7 @@
 def call(Map config = [:]){
     sh 'touch commit_log'
     sh 'git log --pretty=format:"%H %ad" --date=iso $GIT_PREVIOUS_COMMIT..$GIT_COMMIT > commit_log'
+    echo '' >> commit_log
     sh 'cat commit_log'
     sh '''
     #!/bin/bash
@@ -10,18 +11,14 @@ def call(Map config = [:]){
         # Splitting the input text by space to separate commit ID and timestamp
         read -r commit_id timestamp <<< "$input_text"
 
-        # Extracting date and time
-        date="${timestamp%%T*}"
-        time="${timestamp##*T}"
-
-        # Extracting month and day from date
-        month_day=$(echo $date | cut -d'-' -f2,3 | tr '-' '-')
+        formatted_date=$(date -d "$original_date" +"%Y-%m-%dT%H:%M:%S%z")
+        #formatted_date=$(date -j -f "%Y-%m-%d %H:%M:%S %z" "$timestamp" +"%Y-%m-%dT%H:%M:%S%z")
 
         # Constructing the JSON string
-        json_str="{\"commitId\": \"$commit_id\", \"date\":\"$month_day'T'$time\"}"
+        json_str="{\"commitId\": \"$commit_id\", \"date\":\"$formatted_date\"}"
 
         echo $json_str
-    done < "commit_log"
+    done < commit_log
     '''
     sh 'echo Completed'
 }   
